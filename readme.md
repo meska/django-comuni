67f4dc32f678b6eb67e00c18247af75133733a63
= <meskatech@gmail.com>
2017-12-19 11:43:27 +0100

    packaging

--- readme.md
+++ readme.md
@@ -1,6 +1,8 @@
-==========================
 App Django Comuni Italiani
 ==========================
 
 
 Applicazione Django contentente il database dei comuni italiani
+
+
+Testato con python >= 3.6 e django >= 1.11
---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+import sys
+
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
+
+if sys.version_info < (3, 6):
+    sys.exit('Sorry, Python < 3.6 is not supported')
+
+setup(
+    name='django-comuni',
+    version='0.1',
+    install_requires=requirements,
+    url='https://gitlab.com/meska/django-comuni',
+    license='MIT',
+    author='Marco Mescalchin',
+    author_email='meskatech@gmail.com',
+    description='Tabelle Comuni Italiani per Django',
+    packages=['comuni'],
+    package_dir={'comuni': 'comuni'},
+    package_data={'comuni': ['migrations/fixtures/*.*']}
+)
