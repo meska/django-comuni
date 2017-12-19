178f8ebc763011df65fc7d291cf728df2adbb0c3
= <meskatech@gmail.com>
2017-12-19 14:57:32 +0100

    Fix Assegnazione Capoluogo

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ if sys.version_info < (3, 6):
 
 setup(
     name='django-comuni',
-    version='0.2',
+    version='0.2.1',
     install_requires=requirements,
     url='https://gitlab.com/meska/django-comuni',
     license='MIT',
